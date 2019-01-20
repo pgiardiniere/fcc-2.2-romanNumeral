11c7e670db0045140d69956abc1c1317e0e030b6
pgiardiniere <pgiardiniere@gmail.com>
2019-01-20 23:46:44 +0000

    all logic except 9's working

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+"use strict";
+//setup conversion data
 function convertToRoman(num) {
   let roman = [
     { 'numeral': 'I',
@@ -26,28 +28,39 @@ function convertToRoman(num) {
   roman = roman.reverse();
 
   // we know going in that by the way the numerals are structured 
-  // that getting a return of 5 is not possible, since it would trigger a value 1 conversion
-  // on the previous symbol. This also allows 'M' numeral to have no ceiling
+  // that getting a return of 5 is not possible for numbers smaller than 5000, since it would trigger a value 1 conversion
+  // on the previous symbol. This also allows 'M' numeral to have no ceiling. Also, the test data does not go over 2k.
 
+  //array to track numeral during conversion.
+  const numInRoman = [];
 
-  //need to declare an empty array and assign to a variable, this will track our total numeral as it is converted
-  //then in the for loop :: after the if-s, push roman[i].numeral to array
-  //then modify return   :: transform array var toString & return.
-
-  for (i=0; i < roman.length; i++) {
+  for (let i=0; i < roman.length; i++) {
     if (num / roman[i].value >= 4) {
-      //if between 4 and 5, we use the symbol of the current numeral and concatenate the numeral of the higher order after
-      //we subtract 4 * roman[i].value from the num
+      //if between 4 and 5, we push symbol of the current numeral, then push symbol of higher order numeral
+      //then subtract 4 * roman[i].value from the num
+      numInRoman.push(roman[i].numeral, roman[i-1].numeral)
+      num -= 4*roman[i].value;
     }
 
-    //if (logic)
-      //if >= 1 and strictly less than 4, we use between 1 and 3 of the current numeral 
-      //subtract 4 * (between 1 and 3) * roman[i].value
+    if (num / roman[i].value >= 3) {
+      numInRoman.push(roman[i].numeral, roman[i].numeral, roman[i].numeral)
+      num -= 3*roman[i].value;
+    }
 
+    if (num / roman[i].value >= 2) {
+      numInRoman.push(roman[i].numeral, roman[i].numeral);
+      num -= 2*roman[i].value;
+    }
+
+    if (num / roman[i].value >= 1) {
+      numInRoman.push(roman[i].numeral);
+      num -= roman[i].value;
+    }
     
   }
-
-  return num;
+  //toString for return
+  console.log(numInRoman.join(""));
+  return numInRoman.join('');
 }
    
 convertToRoman(36);
