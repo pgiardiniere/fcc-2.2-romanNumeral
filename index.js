99a9dae4988d4257625eba826e0eeec91f374fca
pgiardiniere <pgiardiniere@gmail.com>
2019-01-21 02:58:50 +0000

    Fixed 9s, broke 0s, more tests failing now

--- index.js
+++ index.js
@@ -24,43 +24,37 @@ function convertToRoman(num) {
       'value' : 1000
     }
   ]
-  //sort largest to smallest numerals for easier division
-  roman = roman.reverse();
-
-  // we know going in that by the way the numerals are structured 
-  // that getting a return of 5 is not possible for numbers smaller than 5000, since it would trigger a value 1 conversion
-  // on the previous symbol. This also allows 'M' numeral to have no ceiling. Also, the test data does not go over 2k.
 
   //array to track numeral during conversion.
+  //split num's individual digits into an array of digits
   const numInRoman = [];
+  const arrNum = num.toString().split("").map(Number);
+  console.log(arrNum);
 
-  for (let i=0; i < roman.length; i++) {
-    if (num / roman[i].value >= 4) {
-      //if between 4 and 5, we push symbol of the current numeral, then push symbol of higher order numeral
-      //then subtract 4 * roman[i].value from the num
-      numInRoman.push(roman[i].numeral, roman[i-1].numeral)
-      num -= 4*roman[i].value;
-    }
+  for (let i=0; i < arrNum.length; i++) {
+    //edge cases for 9s - ones, tens, hundreds
+    //nice! finally think I have the EQ. I'll comment out the second two to test it.
+    console.log(i);
+    console.log(roman[i].numeral);
+    console.log(i*2);
+    console.log(roman[i*2].numeral);
+    console.log(i*2+2);
+    console.log(roman[i*2+2].numeral + '\n');
+    //okay so the transposition is behaving as expected
 
-    if (num / roman[i].value >= 3) {
-      numInRoman.push(roman[i].numeral, roman[i].numeral, roman[i].numeral)
-      num -= 3*roman[i].value;
-    }
-
-    if (num / roman[i].value >= 2) {
-      numInRoman.push(roman[i].numeral, roman[i].numeral);
-      num -= 2*roman[i].value;
-    }
-
-    if (num / roman[i].value >= 1) {
-      numInRoman.push(roman[i].numeral);
-      num -= roman[i].value;
-    }
-    
+    if (arrNum[i] === 9) numInRoman.unshift(roman[i*2].numeral, roman[i*2+2].numeral);
+    if (arrNum[i] === 8) numInRoman.unshift(roman[i*2+1].numeral, roman[i*2].numeral, roman[i*2].numeral, roman[i*2].numeral);
+    if (arrNum[i] === 7) numInRoman.unshift(roman[i*2+1].numeral, roman[i*2].numeral, roman[i*2].numeral);
+    if (arrNum[i] === 6) numInRoman.unshift(roman[i*2+1].numeral, roman[i*2].numeral);
+    if (arrNum[i] === 5) numInRoman.unshift(roman[i*2+1].numeral);
+    if (arrNum[i] === 4) numInRoman.unshift(roman[i*2].numeral, roman[i*2+1].numeral);
+    if (arrNum[i] === 3) numInRoman.unshift(roman[i*2].numeral, roman[i*2].numeral, roman[i*2].numeral);
+    if (arrNum[i] === 2) numInRoman.unshift(roman[i*2].numeral, roman[i*2].numeral);
+    if (arrNum[i] === 1) numInRoman.unshift(roman[i*2].numeral);
   }
-  //toString for return
-  console.log(numInRoman.join(""));
+  console.log(numInRoman);
+  console.log(numInRoman.join(''));
   return numInRoman.join('');
 }
    
-convertToRoman(36);
+convertToRoman(99);
