6fc68ea1d6ae16cfe2659e50b471797684e67dcf
pgiardiniere <pgiardiniere@gmail.com>
2019-01-26 22:13:38 +0000

    cases 9, 90, 900 working - not mixed

--- index.js
+++ index.js
@@ -3,58 +3,77 @@
 function convertToRoman(num) {
   let roman = [
     { 'numeral': 'I',
-      'value' : 1
+      'val' : 1
     },
     { 'numeral': 'V',
-      'value' : 5
+      'val' : 5
     },
     { 'numeral': 'X',
-      'value' : 10
+      'val' : 10
     },
     { 'numeral': 'L',
-      'value' : 50
+      'val' : 50
     },
     { 'numeral': 'C',
-      'value' : 100
+      'val' : 100
     },
     { 'numeral': 'D',
-      'value' : 500
+      'val' : 500
     },
     { 'numeral': 'M',
-      'value' : 1000
+      'val' : 1000
     }
   ]
   //sort largest to smallest numerals for easier division
-  roman = roman.reverse();
+  //roman = roman.reverse();
 
-  // we know going in that by the way the numerals are structured 
-  // that getting a return of 5 is not possible for numbers smaller than 5000, since it would trigger a value 1 conversion
-  // on the previous symbol. This also allows 'M' numeral to have no ceiling. Also, the test data does not go over 2k.
+  //don't believe these are necessary - tracks Num in an Array format
+  //const arrNum = num.toString().split("").map(Number);
+  //console.log(arrNum);
+
 
   //array to track numeral during conversion.
   const numInRoman = [];
 
-  for (let i=0; i < roman.length; i++) {
-    if (num / roman[i].value >= 4) {
-      //if between 4 and 5, we push symbol of the current numeral, then push symbol of higher order numeral
-      //then subtract 4 * roman[i].value from the num
-      numInRoman.push(roman[i].numeral, roman[i-1].numeral)
-      num -= 4*roman[i].value;
+  for (let i=roman.length-1; i > -1; i--) {
+    console.log(numInRoman);
+    //handles special case of 9s
+    //push symbol of the current numeral, then push symbol of twice higher order numeral
+    
+    if (num / roman[i].val >= 4 && num % (9*roman[i].val) === 0) {
+      numInRoman.push(roman[i].numeral, roman[i+2].numeral);
+      num -= 9*roman[i].val;
+    }
+    
+    //handles special case of 4s
+    //push symbol of the current numeral, then push symbol of next higher order numeral
+    else if (num / roman[i].val >= 4) {
+      numInRoman.push(roman[i].numeral, roman[i+1].numeral);
+      num -= 4*roman[i].val;
     }
 
-    if (num / roman[i].value >= 3) {
+    if (num / roman[i].val >= 3) {
       numInRoman.push(roman[i].numeral, roman[i].numeral, roman[i].numeral)
-      num -= 3*roman[i].value;
+      num -= 3*roman[i].val;
     }
 
-    if (num / roman[i].value >= 2) {
+    if (num / roman[i].val >= 2) {
       numInRoman.push(roman[i].numeral, roman[i].numeral);
-      num -= 2*roman[i].value;
+      num -= 2*roman[i].val;
     }
 
-    if (num / roman[i].value >= 1) {
+    // && operation is to ensure if value is a 9 that we skip this numeral order
+    //setup a floor function evaluator for it to work
+    //this whole thing is so ugly make this pretty later...
+    let x = 100; 
+    if (i === 2) x=10;
+    if (i === 0) x=1;
+    let numFloored = Math.ceil((num+1)/x*x);
+
+    if (num / roman[i].val >= 1 && (numFloored / roman[i].val === 4 || numFloored === 40 || numFloored ===400)) {
       numInRoman.push(roman[i].numeral);
-      num -= roman[i].value;
+      num -= roman[i].val;
+      console.log(numInRoman);
     }
     
   }
@@ -63,4 +82,4 @@ function convertToRoman(num) {
   return numInRoman.join('');
 }
    
-convertToRoman(36);
+convertToRoman(9);
